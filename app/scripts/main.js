706e77c2112ed1e51fb9661839893faab7ebb9a3
Daniel Torres <daniel.torrer@gmail.com>
2017-02-08 00:27:25 -0600

    landing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(document).ready(function(){
+
+	$(".menu-btn a").click( function(){
+		$(this).toggleClass('menu-btn-back');
+		$('nav').toggleClass('hide');
+		$('body').toggleClass('menu-opened');
+	});
+
+});
