a247345e5fd2d9a4f0bac39145fbd3a57c6577a4
Daniel Alejandro Torres <daniel.torrer@gmail.com>
2017-02-10 17:05:36 -0600

    Landing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-	$(".menu-btn a").click( function(){
+	$('.menu-btn a').click( function(){
 		$(this).toggleClass('menu-btn-back');
 		$('nav').toggleClass('hide');
 		$('body').toggleClass('menu-opened');
