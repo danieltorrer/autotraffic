dd550cc18c5a94abcb05feab9ce98bf541259c38
Daniel Alejandro Torres <daniel.torrer@gmail.com>
2017-02-18 16:08:14 -0600

    stiles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var scrolling = false;
+var position = $(window).scrollTop();
+var currentSlide = 0;
+var minSlide = 0;
+var maxSlide = 2;
+
 $(document).ready(function(){
 
 	$('.menu-btn a').click( function(){
@@ -15,4 +21,76 @@ $(document).ready(function(){
 		e.preventDefault();
 	});
 
+	$(document).on('mousewheel DOMMouseScroll', function(e) {
+
+		if(!scrolling){
+
+			if (event.deltaY > 0){
+				console.log('moving DOWN the page');
+				if(currentSlide <  maxSlide){
+					scrolling =  true;
+					currentSlide++;
+					// $('.scroll-body .slide').not('[data-slide="'+ currentSlide +'"]').addClass('displace');
+					$('.slide[data-slide="'+ currentSlide +'"]').velocity({
+						translateY: '0%',
+					},{
+						duration: 1200,
+						complete: function(){
+							scrolling = false
+						}
+					});
+				}
+			} else {
+				console.log('moving UP the page');
+				// $('.scroll-body .slide').removeClass('animated');
+				if(currentSlide > minSlide){
+					scrolling =  true;
+					currentSlide--;
+					$('.scroll-body .slide').not('[data-slide="'+ currentSlide +'"]')
+					.velocity({ translateY: '100%'}, {duration: 1200,
+						complete: function(){
+							scrolling = false
+						}
+					});
+
+					$('.slide[data-slide="'+ currentSlide +'"]').velocity({
+						translateY: '0%',
+					},{
+						duration: 1200,
+						complete: function(){
+							scrolling = false
+						}
+					});
+				}
+				// $('.scroll-body .slide').not('[data-slide="'+ currentSlide +'"]').addClass('displace');
+				// $('.slide[data-slide="'+ currentSlide +'"]').addClass('animated').removeClass('displace');
+
+
+			}
+
+		}
+
+	});
+
+
 });
+
+
+function stopPlayScroll(e) {
+	if (e) {
+		$(document).on('mousewheel DOMMouseScroll', function(e) {
+			preventDefault(e)
+		});
+		var t = $(document).scrollTop();
+		$(document).on('scroll', function(e) {
+			$(document).scrollTop(t),
+			preventDefault(e)
+		}),
+		$(document).on('touchmove', function(e) {
+			preventDefault(e)
+		})
+	} else
+	$(document).off('mousewheel DOMMouseScroll'),
+	$(document).unbind('scroll'),
+	$(document).off('touchmove')
+}
---
+++ app/scripts/particles.js
@@ -0,0 +1,119 @@
+var resizeReset = function() {
+    canvasBody.width = window.innerWidth;
+    canvasBody.height = window.innerHeight;
+
+    w = canvasBody.width;
+    h = canvasBody.height;
+}
+
+
+
+window.addEventListener('resize', function(){
+    deBouncer();
+});
+
+var deBouncer = function() {
+    clearTimeout(tid);
+    tid = setTimeout(function() {
+        resizeReset();
+    }, delay);
+};
+
+var checkDistance = function(x1, y1, x2, y2){
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+};
+
+var linkPoints = function(point1, hubs){
+    for (var i = 0; i < hubs.length; i++) {
+        var distance = checkDistance(point1.x, point1.y, hubs[i].x, hubs[i].y);
+        var opacity = 1 - distance / opts.linkRadius;
+        if (opacity > 0) {
+            drawArea.lineWidth = 0.5;
+            drawArea.strokeStyle = 'rgba(255, 255, 255, 0.3 )';
+            drawArea.beginPath();
+            drawArea.moveTo(point1.x, point1.y);
+            drawArea.lineTo(hubs[i].x, hubs[i].y);
+            drawArea.closePath();
+            drawArea.stroke();
+        }
+    }
+}
+
+var Particle = function(xPos, yPos){
+    this.x = Math.random() * w;
+    this.y = Math.random() * h;
+    this.speed = opts.defaultSpeed + Math.random() * opts.variantSpeed;
+    this.directionAngle = Math.floor(Math.random() * 360);
+    this.color = opts.particleColor;
+    this.radius = opts.defaultRadius + Math.random() * opts. variantRadius;
+    this.vector = {
+        x: Math.cos(this.directionAngle) * this.speed/2,
+        y: Math.sin(this.directionAngle) * this.speed/2
+    };
+    this.update = function(){
+        this.border();
+        this.x += this.vector.x;
+        this.y += this.vector.y;
+    };
+    this.border = function(){
+        if (this.x >= w || this.x <= 0) {
+            this.vector.x *= -1;
+        }
+        if (this.y >= h || this.y <= 0) {
+            this.vector.y *= -1;
+        }
+        if (this.x > w) this.x = w;
+        if (this.y > h) this.y = h;
+        if (this.x < 0) this.x = 0;
+        if (this.y < 0) this.y = 0;
+    };
+    this.draw = function(){
+        drawArea.beginPath();
+        drawArea.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+        drawArea.closePath();
+        drawArea.fillStyle = this.color;
+        drawArea.fill();
+    };
+};
+
+function setup(){
+    particles = [];
+    resizeReset();
+    for (var i = 0; i < opts.particleAmount; i++){
+        particles.push( new Particle() );
+    }
+    window.requestAnimationFrame(loop);
+}
+
+function loop(){
+    window.requestAnimationFrame(loop);
+    drawArea.clearRect(0,0,w,h);
+    for (var i = 0; i < particles.length; i++){
+        particles[i].update();
+        particles[i].draw();
+    }
+    for (var i = 0; i < particles.length; i++){
+        linkPoints(particles[i], particles);
+    }
+}
+
+var opts = {
+  particleColor: 'rgba(250,250,250, 0.5)',
+  lineColor: 'rgba(250,250,250, 0.5)',
+  particleAmount: 50,
+  defaultSpeed: 1,
+  variantSpeed: 2,
+  defaultRadius: 2,
+  variantRadius: 1,
+  linkRadius: 160,
+};
+
+var canvasBody = document.getElementById('nodes'),
+      drawArea = canvasBody.getContext('2d');
+
+
+var delay = 200, tid, w, h, particles,
+    rgb = opts.lineColor.match(/\d+/g);
+
+resizeReset();
+setup();
