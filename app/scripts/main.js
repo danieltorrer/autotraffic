86ff200dc34cbea9e0a2ad5cf1d7d3e513189e89
Daniel Alejandro Torres <daniel.torrer@gmail.com>
2017-02-21 20:27:44 -0600

    animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,13 +5,49 @@ var position = $(window).scrollTop();
 var currentSlide = 0;
 var minSlide = 0;
 var maxSlide = 3;
+var menuOpened = false;
 
 $(document).ready(function(){
 
+	// $('.news-container').mouseenter(function(e) {
+	// 	$(e.target).find('.button').velocity({
+	// 		'opacity': 1,
+	// 		//'transform': 'scale(1)'
+	// 	}, { duration: 200 })
+	// })
+	// .mouseleave(function(e) {
+	// 	$(e.target).find('.button').velocity({
+	// 		'opacity': 0,
+	// 		//'transform': 'scale(1.5)'
+	// 	}, {duration: 0	});
+	// });
+	
 	$('.menu-btn a').click( function(){
-		$(this).toggleClass('menu-btn-back');
-		$('nav').toggleClass('hide');
+		//$(this).toggleClass('menu-btn-back');
 		$('body').toggleClass('menu-opened');
+		if(menuOpened){
+			$('nav.menu-container').velocity({
+				'translateY': '-130%'
+			},{
+				duration: 0,
+				complete: function(){
+					menuOpened = !menuOpened;
+				}
+			});
+		}
+		else {
+			$('nav.menu-container').velocity({
+				'translateY': '0%'
+			},{
+				duration: 300,
+				easing: 'easeInSine',
+				complete: function(){
+					menuOpened = !menuOpened;
+				}
+			});
+		}
+
+
 	});
 
 	$('.nav-home li').click( function (e) {
@@ -24,88 +60,38 @@ $(document).ready(function(){
 	$('.scroll-body .slide[data-slide="'+ (currentSlide+1) +'"]')
 	.velocity({ translateY: '100%'}, {duration: 0});
 
+	$('nav.menu-container').velocity({'translateY': '-130%'},{duration: 0});
+
 	$(document).on('mousewheel DOMMouseScroll', function(e) {
 
-		if(!scrolling){
+		if(!scrolling && !menuOpened){
 
-			if (event.deltaY > 0){
-				console.log('moving DOWN the page');
-				if(currentSlide <  maxSlide){
-					scrolling =  true;
-					console.log('currentSlide ' + currentSlide);
-					currentSlide++;
-					// $('.scroll-body .slide').not('[data-slide="'+ currentSlide +'"]').addClass('displace');
-					if(currentSlide == maxSlide){
+			if (e.deltaY > 0){
 
-						$('.relative-wrapper').removeClass('fixed');
-						$('.relative-wrapper').velocity({
-							'padding-top': '0vh'
-						},{
-							duration: 1000,
-							easing: "easeOutQuad",
-							complete: function(){
-								scrolling = false;
 
-								$('.scroll-body .slide[data-slide="'+ (currentSlide+1) +'"]')
-								.velocity({ translateY: '100%'}, {duration: 0});
+				console.log('moving UP the page');
 
-								postScrollSettings();
-							}
-						});
+				if(currentSlide == maxSlide && $(window).scrollTop() == 0 ){
+					slideUpLast();
+				}else {
 
-					} else {
-
-						$('.slide[data-slide="'+ currentSlide +'"]').velocity({
-							translateY: '0%'
-						},{
-							duration: 1000,
-							easing: "easeOutQuad",
-							complete: function(){
-								scrolling = false;
-
-								$('.scroll-body .slide[data-slide="'+ (currentSlide+1) +'"]')
-								.velocity({ translateY: '100%'}, {duration: 0});
-
-								postScrollSettings();
-							}
-						});
+					if(currentSlide > minSlide && currentSlide != maxSlide){
+						slideUp();
 					}
 				}
 
 			} else {
-				console.log('moving UP the page');
-				// $('.scroll-body .slide').removeClass('animated');
 
-				if(currentSlide == maxSlide){
-					console.log(event.deltaX, event.deltaY, event.deltaFactor);
+				console.log('moving DOWN the page');
+				if(currentSlide <  maxSlide){
+
+					if(currentSlide == maxSlide-1){
+						slideDownLast();
+
+					} else {
+						slideDown();
+					}
 				}
-
-				if(currentSlide > minSlide){
-					scrolling =  true;
-					console.log('currentSlide ' + currentSlide);
-
-					currentSlide--;
-
-					$('.slide[data-slide="'+ (currentSlide+1) +'"]').velocity({
-						translateY: '100%'
-					},{
-						duration: 1000,
-						easing: "easeOutQuad",
-						complete: function(){
-							scrolling = false;
-
-							postScrollSettings();
-						}
-					});
-					// $('.scroll-body .slide').not('[data-slide="'+ currentSlide +'"]')
-					// .velocity({ translateY: '100%'}, {duration: 0}});
-					//
-
-				}
-				// $('.scroll-body .slide').not('[data-slide="'+ currentSlide +'"]').addClass('displace');
-				// $('.slide[data-slide="'+ currentSlide +'"]').addClass('animated').removeClass('displace');
-
-
 			}
 
 		}
@@ -115,13 +101,90 @@ $(document).ready(function(){
 
 });
 
+function slideDownLast() {
+	currentSlide++;
+	scrolling =  true;
+
+	$('.relative-wrapper').removeClass('fixed');
+	$('.relative-wrapper').velocity({
+		'padding-top': '0vh'
+	},{
+		duration: 1000,
+		easing: 'easeOutQuad',
+		complete: function(){
+			$('.scroll-body .slide[data-slide="'+ (currentSlide+1) +'"]')
+			.velocity({ translateY: '100%'}, {duration: 0});
+			postScrollSettings();
+		}
+	});
+}
+
+function slideUpLast(){
+	currentSlide--;
+	scrolling =  true;
+
+	$('.relative-wrapper').velocity({
+		'padding-top': '100%'
+	}, {
+		duration: 1000,
+		easing: 'easeOutQuad',
+		complete: function(){
+			//$('.scroll-body .slide[data-slide="'+ (currentSlide) +'"]')
+			//.velocity({ translateY: '0%'}, {duration: 0});
+			//$('.relative-wrapper').addClass('fixed');
+			$('.relative-wrapper').addClass('fixed');
+			postScrollSettings();
+		}
+	})
+}
+
+function slideDown(){
+	currentSlide++;
+	scrolling =  true;
+
+	$('.slide[data-slide="'+ currentSlide +'"]').velocity({
+		translateY: '0%'
+	},{
+		duration: 1000,
+		easing: 'easeOutQuad',
+		complete: function(){
+			$('.scroll-body .slide[data-slide="'+ (currentSlide+1) +'"]')
+			.velocity({ translateY: '100%'}, {duration: 0});
+
+			postScrollSettings();
+		}
+	});
+
+}
+
+function slideUp(){
+	scrolling =  true;
+	currentSlide--;
+
+	$('.slide[data-slide="'+ (currentSlide+1) +'"]').velocity({
+		translateY: '100%'
+	},{
+		duration: 1000,
+		easing: 'easeOutQuad',
+		complete: function(){
+			postScrollSettings();
+		}
+	});
+	// $('.scroll-body .slide').not('[data-slide="'+ currentSlide +'"]')
+	// .velocity({ translateY: '100%'}, {duration: 0}});
+	//
+}
+
 
 function postScrollSettings(){
+	scrolling = false;
+
 	if(currentSlide == maxSlide){
 		$(window).scrollTop(0);
 		$('body').removeClass('no-scroll');
 		$('.slide[data-slide="'+ (currentSlide) +'"]').css({position: 'relative'})
 	}
+
 	else {
 		$('body').addClass('no-scroll');
 	}
@@ -144,4 +207,8 @@ function stopPlayScroll(e) {
 	$(document).off('mousewheel DOMMouseScroll'),
 	$(document).unbind('scroll'),
 	$(document).off('touchmove')
+}
+
+function mapValues(value, start1, stop1, start2, stop2) {
+	return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
 }
